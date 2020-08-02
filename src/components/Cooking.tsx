907be3cae916061741a4299168b7ff5a4da45bff
iida nozomi <nozomi@mbp.local>
2020-08-02 20:32:55 +0900

    style cooking

--- src/components/Cooking.tsx
+++ src/components/Cooking.tsx
@@ -1,31 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Button, Container } from '@material-ui/core';
+import {
+  Button,
+  Container,
+  makeStyles,
+  Theme,
+  Divider,
+  Grid,
+} from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { green } from '@material-ui/core/colors';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 const RAKUTEN_API_KEY = process.env.REACT_APP_RAKUTEN_API_KEY;
 
 interface ILargeCategory {
-  parentCategoryId: string
-  categoryId: string,
-  categoryName: string,
-  categoryUrl: string,
-};
+  parentCategoryId: string;
+  categoryId: string;
+  categoryName: string;
+  categoryUrl: string;
+}
 
 interface ICategory {
-  parentCategoryId: string
-  categoryId: string,
-  categoryName: string,
-  categoryUrl: string,
-};
+  parentCategoryId: string;
+  categoryId: string;
+  categoryName: string;
+  categoryUrl: string;
+}
 
 interface ICategories {
-  large: ILargeCategory[] 
-  medium: ICategory[] 
+  large: ILargeCategory[];
+  medium: ICategory[];
   small: ICategory[];
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+  btn: {
+    backgroundColor: green[400],
+    borderColor: green[500],
+    color: '#fff',
+    margin: '5px',
+    '&:hover': {
+      backgroundColor: green[200],
+    },
+    border: 'solid 1px',
+  },
+}));
+
 export default () => {
+  const classes = useStyles();
   const [category, setCategory] = useState<any>([]);
   const [selectedCategory, setSelectedCategory] = useState([]);
   console.log(category);
@@ -57,28 +80,27 @@ export default () => {
   };
 
   return (
-    <Container component='main'>
+    <Grid container component='main' spacing={3}>
       {selectedCategory.map((recipe: ICategory) => (
-        <span key={recipe.categoryId}>
-          {recipe.parentCategoryId ? (
-            <Link
-              to={`cookingLists/${recipe.parentCategoryId}-${recipe.categoryId}`}
-            >
-              <Button variant='contained' color='primary'>
-                {recipe.categoryName}
-              </Button>
-            </Link>
-          ) : (
+        <Grid item key={recipe.categoryId} xs={12} sm={6} >
+          <Link
+            to={
+              recipe.parentCategoryId &&
+              `cookingLists/${recipe.parentCategoryId}-${recipe.categoryId}`
+            }
+          >
             <Button
-              variant='contained'
-              color='primary'
+              size='large'
               onClick={() => onLargeRecipeClick(recipe.categoryId)}
+              // endIcon={<ChevronRightIcon />}
+              fullWidth={true}
             >
               {recipe.categoryName}
             </Button>
-          )}
-        </span>
+            <Divider light={false} />
+          </Link>
+        </Grid>
       ))}
-    </Container>
+    </Grid>
   );
 };
