e76891b87d4590a9c5dfb545ab02ab3609ab2f6a
iida nozomi <nozomi@mbp.local>
2020-08-02 15:44:27 +0900

    make sign up page

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,58 +1,87 @@
-import * as React from 'react';
-import clsx from 'clsx';
-import { useForm } from 'react-hook-form';
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import React, { useState, useContext } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { green } from '@material-ui/core/colors';
+import {
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+} from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import {
-  InputLabel,
-  FormControl,
-  Input,
-  InputAdornment,
-  IconButton,
-  Button,
-  Typography,
-} from '@material-ui/core';
-import firebase from '../firebase/firebase';
+import { useForm } from 'react-hook-form';
 import { Redirect, Link } from 'react-router-dom';
 import { AuthStore } from '../stores/AuthStore';
+import firebase from '../firebase/firebase';
 
 interface FormData {
   name: string;
   email: string;
   password: string;
-  passwordConfirm: string;
+  confirmPassword: string;
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: green[400],
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    color: '#fff',
+    backgroundColor: green[600],
+    '&:hover': {
+      backgroundColor: green[400],
     },
-    margin: {
-      margin: theme.spacing(1),
-    },
-    withoutLabel: {
-      marginTop: theme.spacing(3),
-    },
-    textField: {
-      width: '25ch',
-    },
-  })
-);
+  },
+  linkFont: {
+    color: green[600],
+  },
+}));
 
-export default () => {
+export default function SignUp() {
   const classes = useStyles();
-  const user = React.useContext(AuthStore);
-  const [showPassword, setShowPassword] = React.useState(false);
-  const [showPasswordConfirm, setShowPasswordConfirm] = React.useState(false);
-  const [signUpErr, setSignUpErr] = React.useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const { register, handleSubmit } = useForm<FormData>();
+  const [signUpErr, setSignUpErr] = React.useState('');
+  const user = useContext(AuthStore);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleClickShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  if (user) {
+    return <Redirect to='/' />;
+  }
+
   const onSubmit = handleSubmit(
-    ({ name, email, password, passwordConfirm }) => {
-      if (password === passwordConfirm) {
+    ({ name, email, password, confirmPassword }) => {
+      console.log(email);
+      if (password === confirmPassword) {
         firebase
           .auth()
           .createUserWithEmailAndPassword(email, password)
@@ -61,14 +90,14 @@ export default () => {
               .auth()
               .currentUser?.updateProfile({ displayName: name })
               .then(() => {
-                console.log(firebase.auth().currentUser);
+                console.log('Sing up!!');
               });
           })
           .catch(error => {
             const errorCode = error.code;
             const errorMessage = error.message;
             if (errorCode === 'auth/weak-password') {
-              setSignUpErr('The password is too weak.');
+              setSignUpErr('パスワードは6文字以上にしてください。');
             } else {
               setSignUpErr(errorMessage);
             }
@@ -80,78 +109,113 @@ export default () => {
     }
   );
 
-  if (user) {
-    return <Redirect to='/' />;
-  }
-
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
-  const handleClickShowPasswordConfirm = () => {
-    setShowPasswordConfirm(!showPasswordConfirm);
-  };
-
   return (
-    <>
-      <Typography variant='h4'>アカウントを作成する</Typography>
-      <form onSubmit={onSubmit}>
-        <div className={classes.margin}>
-          <TextField label='名前' name='name' inputRef={register} />
-        </div>
-        <div className={classes.margin}>
-          <TextField label='メールアドレス' name='email' inputRef={register} />
-        </div>
-        <div className={classes.margin}>
-          <FormControl className={clsx(classes.margin, classes.textField)}>
-            <InputLabel htmlFor='standard-adornment-password'>
-              パスワード
-            </InputLabel>
-            <Input
-              type={showPassword ? 'text' : 'password'}
-              name='password'
-              inputRef={register}
-              endAdornment={
-                <InputAdornment position='end'>
-                  <IconButton
-                    aria-label='toggle password visibility'
-                    onClick={handleClickShowPassword}
-                  >
-                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-        </div>
-        <div className={classes.margin}>
-          <FormControl className={clsx(classes.margin, classes.textField)}>
-            <InputLabel htmlFor='standard-adornment-password'>
-              パスワード（確認）
-            </InputLabel>
-            <Input
-              type={showPasswordConfirm ? 'text' : 'password'}
-              name='passwordConfirm'
-              inputRef={register}
-              endAdornment={
-                <InputAdornment position='end'>
-                  <IconButton
-                    aria-label='toggle password visibility'
-                    onClick={handleClickShowPasswordConfirm}
-                  >
-                    {showPasswordConfirm ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-        </div>
-        <Button type='submit' variant='contained' color='primary'>
-          登録
-        </Button>
-      </form>
-      {signUpErr ?? <p>{signUpErr}</p>}
-      <p>すでにアカウントをお持ちですか？</p>
-      <Link to='/login'>ログイン画面へ</Link>
-    </>
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          新規登録
+        </Typography>
+        {signUpErr && <p>{signUpErr}</p>}
+        <form className={classes.form} noValidate onSubmit={onSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                name='name'
+                variant='outlined'
+                required
+                fullWidth
+                id='name'
+                label='名前'
+                autoFocus
+                inputRef={register}
+                autoComplete='user-name'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant='outlined'
+                autoComplete='email'
+                required
+                fullWidth
+                id='email'
+                label='メールアドレス'
+                name='email'
+                inputRef={register}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl variant='outlined' fullWidth required>
+                <InputLabel>パスワード</InputLabel>
+                <OutlinedInput
+                  type={showPassword ? 'text' : 'password'}
+                  name='password'
+                  inputRef={register}
+                  autoComplete='current-password'
+                  endAdornment={
+                    <InputAdornment position='end'>
+                      <IconButton
+                        aria-label='toggle password visibility'
+                        onClick={handleClickShowPassword}
+                        edge='end'
+                      >
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  labelWidth={95}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl variant='outlined' fullWidth required>
+                <InputLabel>パスワード（確認用）</InputLabel>
+                <OutlinedInput
+                  type={showConfirmPassword ? 'text' : 'password'}
+                  name='confirmPassword'
+                  autoComplete='confirm-password'
+                  inputRef={register}
+                  endAdornment={
+                    <InputAdornment position='end'>
+                      <IconButton
+                        aria-label='toggle confirmPassword visibility'
+                        onClick={handleClickShowConfirmPassword}
+                        edge='end'
+                      >
+                        {showConfirmPassword ? (
+                          <Visibility />
+                        ) : (
+                          <VisibilityOff />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  labelWidth={95}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+          >
+            新規登録
+          </Button>
+          <Grid container justify='flex-end'>
+            <Grid item>
+              <Link to='/login' className={classes.linkFont}>
+                ログイン画面へ
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   );
-};
+}
